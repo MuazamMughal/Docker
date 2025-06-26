35710bc6e2d47e77db94b0cd81de61e1a3f869ea
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-06-26 10:02:58 +0500

    conatainerization

--- 02_Containerization/fastapi_containerization/src/fastapi_containerization/main.py
+++ 02_Containerization/fastapi_containerization/src/fastapi_containerization/main.py
@@ -24,3 +24,36 @@ connection_string = str(settings.DATABASE_URL).replace(
 engine = create_engine(
     connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
 )
+
+
+def create_db_and_tables()->None:
+    SQLModel.metadata.create_all(engine)
+
+
+# The first part of the function, before the yield, will
+# be executed before the application starts.
+# https://fastapi.tiangolo.com/advanced/events/#lifespan-function
+@asynccontextmanager
+async def lifespan(app: FastAPI)-> AsyncGenerator[None, None]:
+    print("Creating tables..")
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan, title="Hello World API with DB", 
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server"
+        }
+        ])
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
