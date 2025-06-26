d2356411dac29e4efc8d8daa78e99fb2d4c81d94
muazam mughal <104276759+MuazamMughal@users.noreply.github.com>
2025-06-26 10:02:35 +0500

    conatainerization

---
+++ 02_Containerization/fastapi_containerization/src/fastapi_containerization/main.py
@@ -0,0 +1,26 @@
+# main.py
+from contextlib import asynccontextmanager
+from typing import Union, Optional, Annotated
+from app import settings
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Sequence
+from fastapi import FastAPI, Depends
+from typing import AsyncGenerator
+
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    content: str = Field(index=True)
+
+
+# only needed for psycopg 3 - replace postgresql
+# with postgresql+psycopg in settings.DATABASE_URL
+connection_string = str(settings.DATABASE_URL).replace(
+    "postgresql", "postgresql+psycopg"
+)
+
+
+# recycle connections after 5 minutes
+# to correspond with the compute scale down
+engine = create_engine(
+    connection_string, connect_args={"sslmode": "require"}, pool_recycle=300
+)
